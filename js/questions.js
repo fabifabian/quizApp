becc5e4a16bb385fc0c6c09f0483dcd8f3845b28
fabifabian <55846722+fabifabian@users.noreply.github.com>
2021-05-26 22:03:00 +0200

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,234 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Quelle profession travaille a l'hopital?",
+    answer: "le docteur",
+    options: [
+      "le pompier",
+      "le docteur",
+      "le constructeur",
+      "le cuisinier"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Quelle profession utilise un marteau?",
+    answer: "le constructeur",
+    options: [
+      "le constructeur",
+      "le directeur",
+      "l'infirmiere",
+      "l'astronaute"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Quel travail fait un pompier?",
+    answer: "arrete le feu",
+    options: [
+      "etudie les plantes",
+      "arrete le feu",
+      "guerit les patients",
+      "vole un avion"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Quelle profession utilise un stethoscope?",
+    answer: "le docteur",
+    options: [
+      "le professeur",
+      "le barbier",
+      "le docteur",
+      "le planteur"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Quelle profession a beaucoup de livres?",
+    answer: "le professeur",
+    options: [
+      "le professeur",
+      "l'infirmière",
+      "Le détective",
+      "le conducteur"
+    ]
+  },
+    {
+    numb: 6,
+    question: "Quelle profession construit de nouvelles inventions?",
+    answer: "l'ingénieur",
+    options: [
+      "l'ingénieur",
+      "la police",
+      "le scientifique",
+      "le journaliste"
+    ]
+  },
+    {
+    numb: 7,
+    question: "Quelle profession construit des maisons?",
+    answer: "le constructeur",
+    options: [
+      "l'astronaute",
+      "la police",
+      "le directeur",
+      "le constructeur"
+    ]
+  },
+    {
+    numb: 8,
+    question: "Qui travaille au même endroit que le docteur?",
+    answer: "l'infirmiere",
+    options: [
+      "le professeur",
+      "le pompier",
+      "l'infirmiere",
+      "le cuisinier"
+    ]
+  },
+  {
+    numb: 9,
+    question: "Que fait un pédagogue ?",
+    answer: "enseigne",
+    options: [
+      "enseigne",
+      "construit",
+      "invente",
+      "études"
+    ]
+  },
+  {
+    numb: 10,
+    question: "Voir! Le _____ nourrit les animaux.",
+    answer: "le gardien de zoo",
+    options: [
+      "le gardien de zoo",
+      "l'astronaute",
+      "le détective",
+      "le barbier"
+    ]
+  },
+  {
+    numb: 11,
+    question: "Il veut que le _______ fasse un gâteau spécial.",
+    answer: "le boulanger",
+    options: [
+      "la police",
+      "le boulanger",
+      "le scientifique",
+      "le journaliste"
+    ]
+  },
+  {
+    numb: 12,
+    question: "Le _______ a testé mes yeux et a dit qu'ils est bien.",
+    answer: "l'opticien",
+    options: [
+      "le scientifique",
+      "le pompier",
+      "le constructeur",
+      "l'opticien"
+    ]
+  },
+  {
+    numb: 13,
+    question: "Cette personne fait des peintures:",
+    answer: "l'artiste",
+    options: [
+      "la police",
+      "le professeur",
+      "le cuisinier",
+      "l'artiste"
+    ]
+  },
+  {
+    numb: 14,
+    question: "Cette personne vous servira dans un restaurant:",
+    answer: "le serveur",
+    options: [
+      "le serveur",
+      "Le détective",
+      "le cuisinier",
+      "le directeur"
+    ]
+  },
+  {
+    numb: 15,
+    question: "Cette personne aide les animaux malades:",
+    answer: "le vétérinaire",
+    options: [
+      "l'astronaute",
+      "le barbier",
+      "le vétérinaire",
+      "le docteur"
+    ]
+  },
+  {
+    numb: 16,
+    question: "Cette personne est dans les films et les théâtres :",
+    answer: "l'acteur",
+    options: [
+      "l'acteur",
+      "le dentiste",
+      "le caméraman",
+      "le secrétaire"
+    ]
+  },
+  {
+    numb: 17,
+    question: "Cette personne peut réparer votre voiture:",
+    answer: "la mécanique",
+    options: [
+      "le cuisinier",
+      "la mécanique",
+      "l'ingénieur",
+      "l'électricien"
+    ]
+  },
+  {
+    numb: 18,
+    question: "Cette personne joue d'un instrument:",
+    answer: "",
+    options: [
+      "le vendeur",
+      "la mécanique",
+      "le facteur",
+      "le musicien"
+    ]
+  },
+  {
+    numb: 19,
+    question: "Cette personne fait de la nourriture un restaurant :",
+    answer: "le cuisinier",
+    options: [
+      "l'acteur",
+      "la police",
+      "le cuisinier",
+      "l'artiste"
+    ]
+  },
+  {
+    numb: 20,
+    question: "Cette personne peut vous conduire quelque part:",
+    answer: "le conducteur",
+    options: [
+      "le conducteur",
+      "le boulanger",
+      "le pédagogue",
+      "l'ingénieur"
+    ]
+  },
+  {
+    numb: 21,
+    question: "Cette personne travaille dans un magasin :",
+    answer: "le vendeur",
+    options: [
+      "le vendeur",
+      "l'homme d'affaires",
+      "l'astronaute",
+      "le gardien"
+    ]
+  },
+];
